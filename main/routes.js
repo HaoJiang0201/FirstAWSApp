4ab2dbadb59a48de3f399d95470ad87b473ec6ff
HaoJiang0201 <haojiang0201@gmail.com>
2020-02-18 23:02:21 +0000

    Fisrt Commit

---
+++ main/routes.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+const ENV = process.env.ENV || "development";
+const knexConfig = require("../knexfile");
+const knex = require("knex")(knexConfig[ENV]);
+const knexLogger = require("knex-logger");
+
+router.get('/GetUserName', function (req, res) {
+    console.log("get user name");
+    knex.select().from("users").where('uid', '=', '1')
+        .then((results) => {
+            console.log(results[0]);
+            res.json(results[0]);
+        }).catch(err => console.error(err));
+});
+
+module.exports = router;
